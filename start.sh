0020e8f61921b9db5162172d093ad5b8c23c51c3
Gaétan Ramet <gr@visium.ch>
2020-05-04 15:40:04 +0200

    initial commit

---
+++ start.sh
@@ -0,0 +1,3 @@
+wget https://s3.eu-central-1.wasabisys.com/aicrowd-public-datasets/aicrowd_educational_pkhnd/data/public/test.csv
+wget https://s3.eu-central-1.wasabisys.com/aicrowd-public-datasets/aicrowd_educational_pkhnd/data/public/train.zip
+unzip train.zip
